16b3957c9319aabb6faa5fb254f2341c69bce8c9
Ravi Desai <ravidesai854@gmail.com>
2021-01-31 00:40:40 +0000

    parsed the uevent file, need to add rest of the stats to the stats page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main extends Application {
 
@@ -10,10 +21,44 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Hello World");
-        Scene scene = new Scene(label);
+    public void start(Stage primaryStage){
+        Scene scene = buildScene();
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Battery Monitor");
         primaryStage.show();
     }
+
+    public Scene buildScene(){
+        BorderPane mainPane = new BorderPane();
+        TabPane tabPane = new TabPane();
+        Tab stats = new Tab("Statistics", loadStats());
+        AnchorPane historyPane = new AnchorPane();
+        Tab history = new Tab("History", historyPane);
+        tabPane.getTabs().addAll(stats, history);
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        mainPane.setCenter(tabPane);
+        Scene scene = new Scene(mainPane, 800, 600);
+        return scene;
+    }
+
+    public Pane loadStats(){
+        VBox statsList = new VBox(10);
+        StatisticsLoader loader = new StatisticsLoader();
+        loader.load();
+        HashMap<String, String> statsLoad = loader.getStats();
+        ArrayList<Label> labels = new ArrayList<>();
+        for(Map.Entry<String, String> entry : statsLoad.entrySet()){
+            String key = entry.getKey(); String val = entry.getValue();
+            switch(key){
+                case "percentage":
+                    labels.add(new Label("Percentage: " + val + "%"));
+                    break;
+                    // continue with the rest
+            }
+        }
+        statsList.getChildren().addAll(labels);
+        AnchorPane statsPane = new AnchorPane(statsList);
+        return statsPane;
+    }
+
 }
---
+++ src/main/java/StatisticsLoader.java
@@ -0,0 +1,49 @@
+import javax.print.DocFlavor;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StatisticsLoader {
+    private HashMap<String, String> stats;
+
+    public StatisticsLoader(){
+        stats = new HashMap<>();
+    }
+
+    public void load(){
+        String path = "/sys/class/power_supply/BAT0/uevent";
+        String content = "";
+        try {
+            content = Files.readString(Paths.get(path), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            content = "Failed to retrieve statistics: " + e.toString();
+        }
+        for(String stat : content.split("\n")){
+            String[] pair = stat.split("=");
+            String prefix = pair[0]; String val = pair[1];
+            switch(prefix) {
+                case "POWER_SUPPLY_POWER_NOW":
+                    stats.put("discharge", val);
+                    break;
+                case "POWER_SUPPLY_CAPACITY":
+                    stats.put("percentage", val);
+                    break;
+                case "POWER_SUPPLY_CHARGE_FULL":
+                    stats.put("full_capacity", val);
+                    break;
+                case "POWER_SUPPLY_CHARGE_FULL_DESIGN":
+                    stats.put("design_capacity", val);
+                    break;
+            }
+        }
+    }
+
+    public HashMap<String, String> getStats() {
+        return stats;
+    }
+}
